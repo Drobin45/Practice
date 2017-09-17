1f4b73b7056f02fc995fcea91f64d8fef6d5f234
Daniel <drobin897@gmail.com>
2017-09-17 09:06:54 -0500

    Problem one solved. Problem two begun

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -10,29 +10,49 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-// Problem One. Create a method to see if a string is a palindrome or not. Incomplete
+            // Problem One. Create a method to see if a string is a palindrome or not. Complete!
             string bill = "bill";
             bool answer = CheckPal(bill);
-            Console.WriteLine(answer);
+            //Console.WriteLine(answer);
+
+            // Problem Two. Given an array of numbers, return the highest number in that array
+            int[] blue = { 1, 2, 3, 4, 5, 6 };
+            int highNum = GreatestNum(blue);
+            Console.WriteLine(highNum);
 
 
-            
 
         } //Method separation bracket
 
-        public static bool CheckPal(string inputString)
-        {
-            inputString.Split();
-            if (inputString.Reverse() == inputString)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+        //Problem one method
+        //public static bool CheckPal(string inputString)
+        //{
+        //    inputString.Split();
+        //    if (inputString.Reverse() == inputString)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
 
+        //Problem two method
+
+        public static int GreatestNum(int[] numbers)
+        {
+            for (int i = 0; i >= numbers.Length; i++)
+                if (numbers(i) > numbers(i + 1))
+                    {
+                    continue;
+                    }
+                else if (numbers(i) = numbers.Length)
+                {
+                    return numbers(i);
+                }
         }
 
     }
+
+}
 }
